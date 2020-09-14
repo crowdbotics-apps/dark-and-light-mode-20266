1a55f53cc6578455537d0e02388eaeaa99395b2f
Crowdbotics <team@crowdbotics.com>
2020-09-14 22:35:47 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CustomText, HomePage
+
+admin.site.register(HomePage)
+admin.site.register(CustomText)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,27 +7,55 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(
+        max_length=150,
+    )
+    user = models.TextField(
+        null=True,
+        blank=True,
+    )
+    name = models.CharField(
+        max_length=256,
+        null=True,
+        blank=True,
+    )
+    email = models.CharField(
+        max_length=256,
+        null=True,
+        blank=True,
+    )
+    credit_card = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
 
 
 class HomePage(models.Model):
     body = models.TextField()
+    switch = models.BooleanField(
+        null=True,
+        blank=True,
+    )
+    button = models.BooleanField(
+        null=True,
+        blank=True,
+    )
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
